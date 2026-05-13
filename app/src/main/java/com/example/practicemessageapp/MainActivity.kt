ec88fd9f6fadf9a368931a89316a49a61b46c9b5
itc-s24016 <s24016@std.it-college.ac.jp>
2026-05-13 15:33:41 +0900

    ボタンの処理追加まで これまで▼ ・フォントの変更 ・ボタンの追加

---
+++ app/src/main/java/com/example/practicemessageapp/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.practicemessageapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practicemessageapp.ui.theme.PracticeMessageAppTheme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeMessageAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+// modifier:装飾
+// 画面表示形式を定義している
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // remember:変数の状態を常に保持する(Next.js の useState と似ている)
+    val hello = remember { mutableStateOf("こんにちは") }
+    // 縦に並べて表示する
+    Column(
+        modifier = modifier
+    ) {
+        // 文字を画面に表示する
+        Text(
+//            text = "こんにちは $name!",
+            text = "${hello.value} $name!",
+            fontSize = 32.sp, // 大きさを変える
+            color = Color.Red, // 色を変える
+//        modifier = modifier,
+        )
+        Button(onClick = {
+            hello.value = "こんばんは"
+        }) {
+            Text("夜")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticeMessageAppTheme {
+        Greeting("Android")
+    }
+}
